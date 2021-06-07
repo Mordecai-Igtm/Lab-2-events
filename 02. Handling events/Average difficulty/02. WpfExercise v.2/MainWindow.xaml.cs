4df89e935e08e11310287821b793ef29e7d345ad
Mordecai-Igtm <isaaccooperhack@gmail.com>
2021-06-07 09:30:28 +0300

    Lab-2-events
    Lab-2-events

---
+++ 02. Handling events/Average difficulty/02. WpfExercise v.2/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02._WpfExercise_v._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            // get the name of the drink
+            string login = txtLog.Text;
+            string password = txtPass.Password;
+            if (login.Length <= 1)
+            {
+                log_nope.Visibility = Visibility.Visible;
+                log_ok.Visibility = Visibility.Hidden;
+                MessageBox.Show("ENTER LOGIN!");
+
+
+            }
+            else if (password.Length <= 1)
+            {
+                pass_nope.Visibility = Visibility.Visible;
+                pass_ok.Visibility = Visibility.Hidden;
+                MessageBox.Show("ENTER PASSWORD!");
+
+
+            }
+            else
+            {
+                if (login == "admin")
+                {
+                    log_nope.Visibility = Visibility.Hidden;
+                    log_ok.Visibility = Visibility.Visible;
+
+
+                    if (password == "admin")
+                    {
+                        pass_nope.Visibility = Visibility.Hidden;
+                        pass_ok.Visibility = Visibility.Visible;
+
+                        MessageBox.Show("Welcome back! ");
+
+                    }
+                    else
+                    {
+                        pass_nope.Visibility = Visibility.Visible;
+                        pass_ok.Visibility = Visibility.Hidden;
+                        MessageBox.Show("Wrong Password! Try again! ", "Error");
+                        
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Login! Try again! ", "Error");
+                    log_nope.Visibility = Visibility.Visible;
+                    log_ok.Visibility = Visibility.Hidden;
+                    pass_nope.Visibility = Visibility.Visible;
+                    pass_ok.Visibility = Visibility.Hidden;
+
+                }
+            }
+        }
+    }
+}
---
+++ 02. Handling events/Relatively easy/02. WpfExercise v.1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02._WpfExercise_v._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+            // if name left blank, just exit
+            if (txtName.Text.Length == 0)
+            {
+                MessageBox.Show("You haven't typed in a name!", "Error");
+                txtName.Focus();
+                return;
+            }
+
+            // otherwise, display name
+            MessageBox.Show("Welcome, " + txtName.Text + " from " + txtGroup.Text + " !");
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // close form
+            this.Close();
+
+        }
+    }
+}
